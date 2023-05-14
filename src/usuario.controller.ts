c3c3de0aa32a3acc7dfa0186e1fdda94a94dc5f6
Tiago Costa <tiagodpv@gmail.com>
2023-05-14 16:23:07 -0400

    nest

--- src/usuario.controller.ts
+++ src/usuario.controller.ts
@@ -1,9 +1,13 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsuarioRepository } from './usuario.repository';
 
 @Controller('/usuarios')
 export class usuarioController {
+  private usuarioRepository = new UsuarioRepository();
+
   @Post()
-  async criaUsuario() {
-    return { status: 'usuario criado!' };
+  async criaUsuario(@Body() dadosDoUsuario) {
+    this.usuarioRepository.salvar(dadosDoUsuario);
+    return dadosDoUsuario;
   }
 }
